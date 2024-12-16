55bc1e1cd3b613703e991f5121ad81306a6e8065
JacobJanashvili <ghostiakobilord2@gmail.com>
2024-12-17 01:44:18 +0400

    added github pages config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/construction-company-website/',
   plugins: [
     vue(),
     vueDevTools(),
