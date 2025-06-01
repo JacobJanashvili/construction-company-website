e1f243a035ef8019b73b4e7c49b8e999618c255c
JacobJanashvili <ghostiakobilord2@gmail.com>
2025-06-01 05:46:09 +0400

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
-  base: "/",
+  base: "/construction-company-website/",
   plugins: [vue()],
   resolve: {
     alias: {
