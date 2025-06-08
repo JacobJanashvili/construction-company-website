2d4fca705f1a4312f84d2ed8cbee527ca5636076
JacobJanashvili <ghostiakobilord2@gmail.com>
2025-06-08 20:04:03 +0400

    .

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -52,7 +52,6 @@ const sendEmail = () => {
       },
       (error) => {
         console.log("FAILED...", error);
-        console.log(import.meta.env.VITE_EMAILJS_SERVICEID);
         loading.value = false;
         resultNotification.add({
           severity: "error",
